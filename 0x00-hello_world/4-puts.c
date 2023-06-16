98e229b18bc44e9cd28f1409f13c832882d02140
nthokiesther09 <nthokiesther09@gmail.com>
2023-06-15 17:58:41 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,8 +7,10 @@
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
 }
 
 
