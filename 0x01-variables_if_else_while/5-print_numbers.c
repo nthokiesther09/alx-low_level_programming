1652fc99a12c6e35d44bb86132d74e651b6b8e5d
nthokiesther09 <nthokiesther09@gmail.com>
2023-06-19 15:39:41 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+#include <stdio.h>
+
+
+/**
+ * main - Prints a text according to number.
+ * Return: Always (Success)
+ *
+ */
+
+int main(void)
+
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	printf("\n");
+
+	return (0);
+}
